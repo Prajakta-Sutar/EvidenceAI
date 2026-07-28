4028f85fc3bbf766e02ca5ba569701038c7bd708
Prajakta-Sutar <prajaktas.connect@gmail.com>
2026-07-28 02:18:27 -0600

    Created prompt for multi query retrieval

--- prompts/retrieval_prompt.py
+++ prompts/retrieval_prompt.py
@@ -1,4 +1,18 @@
+from langchain_core.prompts import PromptTemplate
 
+retrieval_prompt = PromptTemplate.from_template("""
+        You are query generator for retrieval.
+        Your job is to create multiple queries that retrieves 
+        different aspects needed to answer the recruiter's questions. 
+        Consider :
+        - Projects
+        - Work experience
+        - technical skills 
+        - education
+        - implementation details 
+
+        Only create retrival queries 
+""")
 
 
 
