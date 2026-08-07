23c5a9375331932f125e843e8196c075d4d877d2
Prajakta-Sutar <prajaktas.connect@gmail.com>
2026-08-07 09:00:58 +0000

    Skills select section is working properly

--- frontend/src/SkillDetails.jsx
+++ frontend/src/SkillDetails.jsx
@@ -1,19 +1,66 @@
 import './SkillDetails.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Spinner from 'react-bootstrap/Spinner';
 
 function SkillDetails({className, setSection, evidence}){
+    const getProject=(path)=>{
+        if (path.includes("askmentor")) {
+            return "AskMentor";
+        }
+        else if (path.includes("datagenesys")) {
+            return "DataPredictify";
+        }
+        else if (path.includes("EvidenceAI")) {
+            return "EvidenceAI"
+        }
+        else{
+            return null; 
+        }
+    }
+
+    const getAbsolutePath=(path)=>{
+        return path.replace("./evidence/repository/", "");
+    }
+
+
     return(
         <div className={className}>
-            <span className='evidence_heading'>Evidence to Support</span>
+                {evidence.length === 0 ? (
+                    <span className='evidence_heading'>
+                        <p className='loading_text'>Loading evidence....</p>
+                        <Spinner animation="border" className='loading_icon'/>
+                    </span>
+                ) : (
+                    <span className='evidence_heading'>
+                        <p className='evidence_heading_text'>Evidences to Support</p>
+                    </span>
+                )}
                 {evidence.map((item, index) => (
-                    <div className='evidence_item'>
-                        <p key={index}>{item.file}</p>
-                        <p>{item.description}</p>
+                    <div className='evidence_item' key={index}>
+                        <Row>
+                            <Col className='project_name'>{getProject(item.file)} Project</Col>
+                        </Row>
+                        <Row>
+                            <Col xs="auto" className='evidence_item_heading'> # File : </Col>
+                        </Row>
+                        <Row>
+                            <Col>{getAbsolutePath(item.file)}</Col>
+                        </Row>
+                        <Row>
+                            <Col xs="auto" className='evidence_item_heading'> # Description : </Col>
+                        </Row>
+                        <Row>
+                            <Col>{item.description}</Col>
+                        </Row>
                         <div className='evidence_code'>
                             <pre>
                                 {item.code}
                             </pre>
                         </div>
+                        <hr className="line"/>
                     </div>
+                    
                 ))}
         </div>
     );
