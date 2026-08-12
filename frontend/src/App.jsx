d03f9afbf4c3bb069382a5c4a59b7f26131db34f
Prajakta-Sutar <prajaktas.connect@gmail.com>
2026-08-12 22:02:09 +0000

    Finally github actions workflow is working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,7 +28,6 @@ function App(){
   const [skill, setSkill] = useState({name:"", id:0});
   const [project, setProject] = useState("");
   const [evidence, setEvidence] = useState([]);
-  const intro = "Testing"
   const [conversation, setConversation] = useState([]);
   const [lastSection, setLastSection] = useState("portfolio");
 
