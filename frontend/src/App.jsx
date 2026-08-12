53a70b2907d58c934575a39b513f267a400eb88a
Prajakta-Sutar <prajaktas.connect@gmail.com>
2026-08-12 21:38:30 +0000

    Testing the workflow again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,7 +28,7 @@ function App(){
   const [skill, setSkill] = useState({name:"", id:0});
   const [project, setProject] = useState("");
   const [evidence, setEvidence] = useState([]);
-  const intro_message = "Hello, I am just testing the workflow"
+  const testing = "Hello Testing workflow"
   const [conversation, setConversation] = useState([]);
   const [lastSection, setLastSection] = useState("portfolio");
 
